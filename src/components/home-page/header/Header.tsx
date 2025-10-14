898d0c37ef569f8fe95b01da82d178b7d27549ec
ArmanGrigorian <armangrigorian.work@gmail.com>
2025-10-14 13:19:56 +0400

    update: max-width of container

--- src/components/home-page/header/Header.tsx
+++ src/components/home-page/header/Header.tsx
@@ -3,7 +3,7 @@ import Paragraph from "@/components/shared/Paragraph";
 
 export default function Header() {
   return (
-    <header className="max-w-full-hd bg-home mx-auto flex min-h-dvh flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-6">
+    <header className="max-w-2k bg-home mx-auto flex min-h-dvh flex-col items-center justify-center bg-cover bg-center bg-no-repeat p-6">
       <hgroup className="rounded-xl border-3 border-primary p-5 text-center shadow-xl backdrop-blur-[2px] lg:p-7">
         <Heading tag="h1" className="text-shadow-lg font-secondary">
           Welcome to Bella Vista
--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarLink from "./NavbarLink";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-full-hd fixed right-0 left-0 z-50 mx-auto w-full bg-green-900/80 px-6 py-5 backdrop-blur-xs">
+    <nav className="max-w-2k fixed right-0 left-0 z-50 mx-auto w-full bg-green-900/80 px-6 py-5 backdrop-blur-xs">
       <ul className="flex items-center gap-4">
         {NAVBAR_LINKS.map((link) => (
           <NavbarLink key={link.id} link={link} />
